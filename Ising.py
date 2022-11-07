bb1a70b227d7548285c303106c5ac1d4e0f3bab2
Miguel Aguilera <sci@maguilera.net>
2022-11-07 17:11:30 +0100

    Update Ising.py

--- Ising.py
+++ Ising.py
@@ -44,7 +44,7 @@ class Ising:
 		if i is None:
 			i = np.random.randint(self.size)
 		h = 2*self.s[i]*(self.H[i] + np.dot(self.J[i,:]+self.J[:,i],self.s))
-		self.s[i] = int(random.rand() < np.tanh(self.Beta*h))    # Glauber
+		self.s[i] = int(random.rand()*2-1 < np.tanh(self.Beta*h))    # Glauber
 
 	def SequentialGlauberStep(self):
 		for i in np.random.permutation(self.size):
