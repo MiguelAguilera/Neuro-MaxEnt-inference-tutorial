893b0b8a1914bfce8b9528b14a96292201fbd5a9
Miguel Aguilera <sci@maguilera.net>
2022-11-14 11:41:20 +0100

    Create kinetic_Ising.py

---
+++ kinetic_Ising.py
@@ -0,0 +1,25 @@
+import numpy as np
+from itertools import combinations
+import matplotlib.pyplot as plt
+
+class Ising:
+	def __init__(self, netsize):	#Create ising model
+	
+		self.size=netsize
+		self.H=np.zeros(netsize)
+		self.J=np.zeros((netsize,netsize))
+		self.randomize_state()
+		self.Beta=1.0
+	
+	def randomize_state(self):
+		self.s = np.random.randint(0,2,self.size)*2-1
+		
+	def GlauberStep(self,i=None):			#Execute step of Glauber algorithm
+		if i is None:
+			i = np.random.randint(self.size)
+		h = self.H[i] + np.dot(self.J,self.s)
+		self.s[i] = int(np.random.rand()*2-1 < np.tanh(self.Beta*h))    # Glauber
+
+	def SequentialGlauberStep(self):
+		for i in np.random.permutation(self.size):
+			self.GlauberStep(i)
